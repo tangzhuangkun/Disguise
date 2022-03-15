0438201b7b675d5aedf96a051bb4b56fffe7f523
AdhocSun <1029791755@qq.com>
2022-03-15 23:51:00 +0800

    完善 伪装模块的输出，完成

--- disguise_controller.py
+++ disguise_controller.py
@@ -5,7 +5,7 @@
 import sys
 sys.path.append('..')
 import parser.disguise as disguise
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,13 +15,60 @@ def hello():
 @app.route('/get_one_ip_and_ua', methods=['GET'])
 def get_one_ip_and_ua():
     '''
-    获取一个IP和一个UA
+    根据协议类型，从数据库中获取一个IP和一个UA
+    :param protocol: 协议类型，默认 https, 也可选择http
     :return:
-    返回例如：{'ip_address': '218.4.193.22:55834'} {'ua': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'}
+    如 {
+    "IP": "117.157.197.18:3128",
+    "UA": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
+    }
     '''
-    ip, ua = disguise.Disguise().get_one_IP_UA()
+    # 协议类型，默认 https, 也可选择http
+    protocol = request.args.get('protocol')
+
+    ip, ua = disguise.Disguise().get_one_IP_UA(protocol)
     # 返回处理成 json格式
-    return jsonify({"ip": ip["ip_address"], "ua":ua["ua"]})
+    return jsonify({"IP": ip["ip_address"], "UA":ua["ua"]})
+
+@app.route('/get_multi_IP_UA', methods=['GET'])
+def get_multi_IP_UA():
+    '''
+    根据协议类型，从数据库中获取多个IP和多个UA
+    :param protocol: 协议类型，默认 https, 也可选择http
+    :param num: 需要多少个IP和UA， 实际输出可能比num少
+    :return:
+    如 {
+    "IPs": [
+        "113.214.48.5:8000",
+        "117.157.197.18:3128",
+        "118.190.244.234:3128"
+    ],
+    "UAs": [
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:21.0) Gecko/20100101 Firefox/21.0",
+        "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:27.0) Gecko/20121011 Firefox/27.0",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36"
+    ]
+}
+    '''
+    # 协议类型，默认 https, 也可选择http
+    num = request.args.get('num')
+    # 协议类型，默认 https, 也可选择http
+    protocol = request.args.get('protocol')
+
+    # 获取多个IP 和 UA
+    ip_address_dict_list,ua_dict_list = disguise.Disguise().get_multi_IP_UA(num,protocol)
+    # 将IP和UA从dict转为list，便于输出成json
+    ip_address_list = []
+    ua_list = []
+    for address in ip_address_dict_list:
+        ip_address_list.append(address["ip_address"])
+    for ua in ua_dict_list:
+        ua_list.append(ua["ua"])
+
+    # 返回处理成 json格式
+    return jsonify(IPs = ip_address_list, UAs=ua_list)
 
 if __name__ == '__main__':
     app.run()
--- parser/disguise.py
+++ parser/disguise.py
@@ -13,13 +13,20 @@ class Disguise:
 	def __init__(self):
 		pass
 	
-	def get_one_IP_UA(self):
-		# 从数据库中获取一个IP和一个UA
-		# 返回：一个IP和一个UA
-		# 返回例如：{'ip_address': '218.4.193.22:55834'} {'ua': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'}
+	def get_one_IP_UA(self, protocol='https'):
+		'''
+		根据协议类型，从数据库中获取一个IP和一个UA
+		:param protocol: 协议类型，默认 https, 也可选择http
+		:return: 如 {'ip_address': '218.4.193.22:55834'} {'ua': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'}
+		'''
 		
 		# 获取IP
-		ip_address_sql = "SELECT ip_address FROM IP_availability ORDER BY RAND() LIMIT 1"
+		ip_address_sql = None
+		if(protocol=='https'):
+			ip_address_sql = "SELECT ip_address FROM IP_availability WHERE is_https = 1 ORDER BY RAND() LIMIT 1"
+		elif(protocol=='http'):
+			ip_address_sql = "SELECT ip_address FROM IP_availability WHERE is_http = 1 ORDER BY RAND() LIMIT 1"
+		print(ip_address_sql)
 		ip_address = db_operator.DBOperator().select_one('parser_component',ip_address_sql)
 		
 		# 获取UA
@@ -29,17 +36,23 @@ class Disguise:
 		return ip_address,ua
 		
 
-	def get_multi_IP_UA(self,num):
-		# 从数据库中获取多个IP和多个UA
-		# num: 需要多少个IP和UA
-		# 返回：多个IP和多个UA
-		# 返回例如：[{'ip_address': '101.255.125.10:8080'}, {'ip_address': '103.124.89.221:55443'}][{'ua': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}, {'ua': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'}]
+	def get_multi_IP_UA(self,num,protocol='https'):
+		'''
+		根据协议类型，从数据库中获取多个IP和多个UA
+		:param num: 需要多少个IP和UA
+		:param protocol: 协议类型，默认 https, 也可选择http
+		:return: 多个IP和多个UA, 可能满足条件的IP会少于num
+		如：[{'ip_address': '101.255.125.10:8080'}, {'ip_address': '103.124.89.221:55443'}][{'ua': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36'}, {'ua': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36'}]
+		'''
 		
 		# 获取多个IP
-		ip_address_sql = "SELECT ip_address FROM IP_availability LIMIT %s" %(str(num))
+		ip_address_sql = None
+		if (protocol == 'https'):
+			ip_address_sql = "SELECT ip_address FROM IP_availability WHERE is_https = 1 LIMIT %s" %(str(num))
+		elif (protocol == 'http'):
+			ip_address_sql = "SELECT ip_address FROM IP_availability WHERE is_http = 1 LIMIT %s" % (str(num))
 		ip_address_dict_list = db_operator.DBOperator().select_all('parser_component',ip_address_sql)
 
-		
 		# 获取多个UA
 		ua_sql = "SELECT ua FROM fake_user_agent LIMIT %s" %(str(num))
 		ua_dict_list = db_operator.DBOperator().select_all('parser_component',ua_sql)
@@ -49,8 +62,8 @@ class Disguise:
 
 if __name__ == "__main__":
 	go = Disguise()
-	ip_address,ua = go.get_one_IP_UA()
-	print(ip_address,ua)
-	#ip_address_dict_list,ua_dict_list = go.get_multi_IP_UA(2)
-	#print(ip_address_dict_list,ua_dict_list)
+	#ip_address,ua = go.get_one_IP_UA('http')
+	#print(ip_address,ua)
+	ip_address_dict_list,ua_dict_list = go.get_multi_IP_UA(10)
+	print(ip_address_dict_list,ua_dict_list)
 		
